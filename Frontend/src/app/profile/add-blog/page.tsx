dbe22aad803eaea48c8fd03571dd6e488c7da147
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-12-14 07:18:54 +0600

    need add blog page

---
+++ Frontend/src/app/profile/add-blog/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface pageProps {
+
+};
+
+const page: FC<pageProps> = ({ }) => {
+    return (
+        <div>
+            page add blog.
+        </div>
+    );
+};
+
+export default page;
--- Frontend/src/components/Blog.tsx
+++ Frontend/src/components/Blog.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import { FC } from 'react';
+import { TiDocumentAdd } from "react-icons/ti";
 
 interface BlogProps {
 
@@ -7,7 +9,18 @@ interface BlogProps {
 const Blog: FC<BlogProps> = ({ }) => {
     return (
         <div>
-            Blog
+            <div className='flex justify-between items-center'>
+                <h3 className='text-xl'> Blog</h3>
+                <Link
+                    href="/profile/add-blog"
+                    className="flex justify-center md:justify-start font-medium gap-x-2 shadow-[0_0px_15px_rgba(0,0,0,0.15)] hover:opacity-90 transform py-5 rounded-full my-5 text-xl"
+                >
+                    <TiDocumentAdd
+                        className="h-8 w-8"
+                    />
+                    <span>Add Blog</span>
+                </Link>
+            </div>
         </div>
     );
 };
