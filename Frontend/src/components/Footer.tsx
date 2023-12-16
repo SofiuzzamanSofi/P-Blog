03e5a1b114cc43ef09a1db5c23e8feadbbaa53a0
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-12-17 04:05:36 +0600

    sign in page footer hide

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import { FC } from 'react';
+import { usePathname } from "next/navigation";
 
 interface FooterProps {
 
 };
 
 const Footer: FC<FooterProps> = ({ }) => {
+
+    const pathname = usePathname() ?? "";
+
+    // jsx code 
+    if (pathname.includes("sign-up") || pathname.includes("sign-in")) {
+        return null;
+    }
+
     return (
         <div className='text-center text-xs bg-gray-900 py-4'>
             <p>Developed by : Software Team, Sofi Tech Bangladesh.</p>
