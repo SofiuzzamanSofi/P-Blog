b825c99d7e192cdbdc1f9a15a0990bc6389b9822
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-12-27 03:04:30 +0600

    edit homepage-7

--- Frontend/src/components/Modals/ProfileDetailsModal.tsx
+++ Frontend/src/components/Modals/ProfileDetailsModal.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-
 import { BiSearch } from "react-icons/bi";
 import { useAuth } from "@/provider/AuthProvider";
 import ProfileModal from "./ProfileModal";
